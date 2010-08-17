51cc11de43552cc1406d4600630a8fdbeebeb596
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2010-08-17 21:43:55 +0000

    Added GLSL files.
    git-svn-id: https://xreal.svn.sourceforge.net/svnroot/xreal/branches/ET-XreaL@3910 8d6dca30-e10d-0410-ba00-f2efb4c278cc

---
+++ etmain/glsl/vertexLighting_DBS_world_vp.glsl
@@ -0,0 +1,180 @@
+/*
+===========================================================================
+Copyright (C) 2008-2009 Robert Beckebans <trebor_7@users.sourceforge.net>
+
+This file is part of XreaL source code.
+
+XreaL source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+XreaL source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XreaL source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+
+/* vertexLighting_DBS_world_vp.glsl */
+
+attribute vec4		attr_Position;
+attribute vec4		attr_TexCoord0;
+attribute vec3		attr_Tangent;
+attribute vec3		attr_Binormal;
+attribute vec3		attr_Normal;
+attribute vec4		attr_Color;
+#if !defined(COMPAT_Q3A)
+attribute vec3		attr_LightDirection;
+#endif
+
+uniform mat4		u_DiffuseTextureMatrix;
+uniform mat4		u_NormalTextureMatrix;
+uniform mat4		u_SpecularTextureMatrix;
+uniform int			u_InverseVertexColor;
+uniform mat4		u_ModelViewProjectionMatrix;
+
+uniform int			u_DeformGen;
+uniform vec4		u_DeformWave;	// [base amplitude phase freq]
+uniform float		u_DeformSpread;
+uniform float		u_Time;
+
+uniform int			u_ColorGen;
+uniform int			u_AlphaGen;
+uniform vec4		u_Color;
+
+varying vec3		var_Position;
+varying vec4		var_TexDiffuseNormal;
+varying vec2		var_TexSpecular;
+varying vec4		var_LightColor;
+#if !defined(COMPAT_Q3A)
+varying vec3		var_LightDirection;
+#endif
+varying vec3		var_Tangent;
+varying vec3		var_Binormal;
+varying vec3		var_Normal;
+
+
+// for construction of a triangle wave
+float triangle(float x)
+{
+	return max(1.0 - abs(x), 0);
+}
+
+float sawtooth(float x)
+{
+	return x - floor(x);
+}
+
+vec4 DeformPosition(const vec4 pos, const vec3 normal)
+{
+	vec4 deformed = pos;
+	
+	/*
+		define	WAVEVALUE( table, base, amplitude, phase, freq ) \
+			((base) + table[ Q_ftol( ( ( (phase) + backEnd.refdef.floatTime * (freq) ) * FUNCTABLE_SIZE ) ) & FUNCTABLE_MASK ] * (amplitude))
+	*/
+
+	if(u_DeformGen == DGEN_WAVE_SIN)
+	{
+		float off = (pos.x + pos.y + pos.z) * u_DeformSpread;
+		float scale = u_DeformWave.x  + sin(off + u_DeformWave.z + (u_Time * u_DeformWave.w)) * u_DeformWave.y;
+		vec3 offset = normal * scale;
+
+		deformed.xyz += offset;
+	}
+	
+	if(u_DeformGen == DGEN_WAVE_SQUARE)
+	{
+		float off = (pos.x + pos.y + pos.z) * u_DeformSpread;
+		float scale = u_DeformWave.x  + sign(sin(off + u_DeformWave.z + (u_Time * u_DeformWave.w))) * u_DeformWave.y;
+		vec3 offset = normal * scale;
+
+		deformed.xyz += offset;
+	}
+	
+	if(u_DeformGen == DGEN_WAVE_TRIANGLE)
+	{
+		float off = (pos.x + pos.y + pos.z) * u_DeformSpread;
+		float scale = u_DeformWave.x  + triangle(off + u_DeformWave.z + (u_Time * u_DeformWave.w)) * u_DeformWave.y;
+		vec3 offset = normal * scale;
+
+		deformed.xyz += offset;
+	}
+	
+	if(u_DeformGen == DGEN_WAVE_SAWTOOTH)
+	{
+		float off = (pos.x + pos.y + pos.z) * u_DeformSpread;
+		float scale = u_DeformWave.x  + sawtooth(off + u_DeformWave.z + (u_Time * u_DeformWave.w)) * u_DeformWave.y;
+		vec3 offset = normal * scale;
+
+		deformed.xyz += offset;
+	}
+	
+	if(u_DeformGen == DGEN_WAVE_INVERSE_SAWTOOTH)
+	{
+		float off = (pos.x + pos.y + pos.z) * u_DeformSpread;
+		float scale = u_DeformWave.x + (1.0 - sawtooth(off + u_DeformWave.z + (u_Time * u_DeformWave.w))) * u_DeformWave.y;
+		vec3 offset = normal * scale;
+
+		deformed.xyz += offset;
+	}
+
+	return deformed;
+}
+
+void	main()
+{
+	vec4 position = DeformPosition(attr_Position, attr_Normal);
+
+	// transform vertex position into homogenous clip-space
+	gl_Position = u_ModelViewProjectionMatrix * position;
+	
+	// assign position in object space
+	var_Position = position.xyz;
+	
+	// transform diffusemap texcoords
+	var_TexDiffuseNormal.st = (u_DiffuseTextureMatrix * attr_TexCoord0).st;
+	
+#if defined(r_NormalMapping)
+	// transform normalmap texcoords
+	var_TexDiffuseNormal.pq = (u_NormalTextureMatrix * attr_TexCoord0).st;
+	
+	// transform specularmap texture coords
+	var_TexSpecular = (u_SpecularTextureMatrix * attr_TexCoord0).st;
+#endif
+	
+#if !defined(COMPAT_Q3A)
+	// assign vertex to light vector in object space
+	var_LightDirection = attr_LightDirection;
+#endif
+	
+	// assign color
+	if(u_ColorGen == CGEN_VERTEX)
+	{
+		var_LightColor.r = attr_Color.r;
+		var_LightColor.g = attr_Color.g;
+		var_LightColor.b = attr_Color.b;
+	}
+	else if(u_ColorGen == CGEN_ONE_MINUS_VERTEX)
+	{
+		var_LightColor.r = 1.0 - attr_Color.r;
+		var_LightColor.g = 1.0 - attr_Color.g;
+		var_LightColor.b = 1.0 - attr_Color.b;
+	}
+	else
+	{
+		var_LightColor.rgb = u_Color.rgb;
+	}
+	
+#if defined(r_NormalMapping)
+	var_Tangent = attr_Tangent;
+	var_Binormal = attr_Binormal;
+#endif
+
+	var_Normal = attr_Normal;
+}
