18af221ca666ad58a5def776109f49785742ba13
trebor_7 <trebor_7@users.sourceforge.net>
2010-09-07 15:31:18 +0200

    Adapted indent_*.sh scripts.

--- indent_game.sh
+++ indent_game.sh
@@ -1,30 +1,10 @@
 #!/bin/sh
 
-for i in `find src/botai/ -iname "*.h"`; do dos2unix $i; done
-for i in `find src/botai/ -iname "*.c"`; do dos2unix $i; done
+for i in `find etmain/src/ -iname "*.h"`; do dos2unix $i; done
+for i in `find etmain/src/ -iname "*.c"`; do dos2unix $i; done
 
-for i in `find src/cgame/ -iname "*.h"`; do dos2unix $i; done
-for i in `find src/cgame/ -iname "*.c"`; do dos2unix $i; done
-
-for i in `find src/game/ -iname "*.h"`; do dos2unix $i; done
-for i in `find src/game/ -iname "*.c"`; do dos2unix $i; done
-
-for i in `find src/ui/ -iname "*.h"`; do dos2unix $i; done
-for i in `find src/ui/ -iname "*.c"`; do dos2unix $i; done
-
-
-for i in `find src/botai/ -iname "*.h"`; do ./indent.exe $i; done
-for i in `find src/botai/ -iname "*.c"`; do ./indent.exe $i; done
-
-for i in `find src/cgame/ -iname "*.h"`; do ./indent.exe $i; done
-for i in `find src/cgame/ -iname "*.c"`; do ./indent.exe $i; done
-
-for i in `find src/game/ -iname "*.h"`; do ./indent.exe $i; done
-for i in `find src/game/ -iname "*.c"`; do ./indent.exe $i; done
-
-for i in `find src/ui/ -iname "*.h"`; do ./indent.exe $i; done
-for i in `find src/ui/ -iname "*.c"`; do ./indent.exe $i; done
-
+for i in `find etmain/src/ -iname "*.h"`; do ./indent.exe $i; done
+for i in `find etmain/src/ -iname "*.c"`; do ./indent.exe $i; done
 
 for i in `find -iname "*~"`; do rm $i; done
 for i in `find -iname "*~"`; do rm $i; done
