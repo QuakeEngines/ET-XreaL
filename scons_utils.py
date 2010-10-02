fc8443c34fadae5432491abb2710e27ee702fc7b
trebor_7 <trebor_7@users.sourceforge.net>
2010-10-02 13:46:57 +0200

    Fixed deprecated python stuff

--- scons_utils.py
+++ scons_utils.py
@@ -1,5 +1,5 @@
 # -*- mode: python -*-
-import sys, os, string, time, commands, re, pickle, StringIO, popen2, commands, pdb, zipfile
+import sys, os, string, time, commands, re, pickle, StringIO, subprocess, commands, pdb, zipfile
 import SCons
 
 # need an Environment and a matching buffered_spawn API .. encapsulate
