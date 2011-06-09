d696b6acdb7fd3d7fdc7dcbee07e19cd086f6c28
trebor_7 <trebor_7@users.sourceforge.net>
2011-06-10 01:01:01 +0200

    Added dedicated server target.

--- src/engine/premake4.lua
+++ src/engine/premake4.lua
@@ -7,8 +7,6 @@ project "ETXreaL"
 	flags       { "ExtraWarnings" }
 	files
 	{
-		"*.txt", 
-		
 		"../shared/**.c", "../shared/**.h",
 		
 		"botlib/**.c", "botlib/**.h",
@@ -229,10 +227,6 @@ project "ETXreaL"
 			"unix/linux_gl3imp.c",
 			"../libs/glew/src/glew.c",
 		}
-		includedirs
-		{
-			"code/glfw/lib/x11",
-		}
 		--buildoptions
 		--{
 		--	"-pthread"
@@ -240,6 +234,180 @@ project "ETXreaL"
 		links
 		{
 			"GL",
+		}
+		defines
+		{
+            "PNG_NO_ASSEMBLER_CODE",
+		}
+
+
+
+
+project "ETXreaL-dedicated"
+	targetname  "ETXreaL-dedicated"
+	language    "C++"
+	kind        "WindowedApp"
+	targetdir 	"../.."
+	flags       { "ExtraWarnings" }
+	files
+	{
+		"../shared/**.c", "../shared/**.h",
+		
+		"botlib/**.c", "botlib/**.h",
+		"server/**.c", "server/**.h",
+		
+		"splines/**.cpp", "splines/**.h",
+
+		"null/null_client.c",
+		"null/null_input.c",
+		"null/null_snddma.c",
+		
+		"qcommon/**.h", 
+		"qcommon/cmd.c",
+		"qcommon/common.c",
+		"qcommon/cvar.c",
+		"qcommon/files.c",
+		"qcommon/huffman.c",
+		"qcommon/md4.c",
+		"qcommon/msg.c",
+		"qcommon/vm.c",
+		"qcommon/vm_interpreted.c",
+		"qcommon/net_*.c",
+		"qcommon/cm_*.c",
+		"qcommon/unzip.c",
+		
+		"qcommon/dl_main_stubs.c",
+		"qcommon/dl_**.h",
+		
+		"../libs/zlib/**.c", "../../libs/zlib/**.h",
+	}
+	includedirs
+	{
+		"../libs/zlib",
+	}
+	defines
+	{ 
+		"DEDICATED",
+		"BOTLIB",
+		--"USE_CURL", 
+		--"USE_CODEC_VORBIS", 
+		--"USE_CIN_THEORA",
+		--"USE_MUMBLE",
+		--"USE_VOIP",
+		--"USE_SSE=2",
+		--"USE_INTERNAL_GLFW",
+		--"USE_INTERNAL_GLEW",
+		--"PRODUCT_VERSION=\\\"ioquake3-trim-1.36\\\"",
+	}
+	excludes
+	{
+		"botlib/botlib_stub.c",
+		"server/sv_rankings.c",
+	}
+	
+	--
+	-- Platform Configurations
+	-- 	
+	configuration "x86"
+		files
+		{ 
+			"code/qcommon/vm_x86.c",
+		}
+	
+	--configuration "x64"
+	--	files
+	--	{ 
+	--		"qcommon/vm_x86_64.c",
+	--		"qcommon/vm_x86_64_assembler.c",
+	--	}
+	
+	--
+	-- Release/Debug Configurations
+	--
+	configuration "Debug"
+		defines     "_DEBUG"
+		flags       { "Symbols", "StaticRuntime" }
+	
+	configuration "Release"
+		defines     "NDEBUG"
+		flags       { "OptimizeSpeed", "StaticRuntime" }
+	
+	-- 
+	-- Project Configurations
+	-- 
+	configuration "vs*"
+		flags       { "WinMain" }
+		files
+		{
+			"win32/**.c", "win32/**.cpp", "win32/**.h",
+			"win32/winquake.rc",
+			"win32/win_gamma.c",
+			"win32/win_gl3imp.c",
+			
+			"../libs/glew/src/wglew.h",
+		}
+		excludes
+		{
+			--"win32/win_gamma.c",
+			--"win32/win_glimp.c",
+		}
+		libdirs
+		{
+			--"../libs/curl-7.12.2/lib"
+		}
+		links
+		{ 
+			"winmm",
+			"wsock32",
+			"Iphlpapi",
+		}
+		--linkoptions
+		--{
+		--	"/NODEFAULTLIB:libc"
+		--}
+		buildoptions
+		{
+			"/EHa",
+			"/fp:fast",
+			"/arch:SSE"
+		}
+		defines
+		{
+			"WIN32",
+			"_CRT_SECURE_NO_WARNINGS",
+			--"USE_INTERNAL_SPEEX",
+			--"USE_INTERNAL_ZLIB",
+			--"FLOATING_POINT",
+			--"USE_ALLOCA"
+		}
+
+	configuration { "linux", "gmake" }
+		buildoptions
+		{
+			--"`pkg-config --cflags sdl`",
+		}
+		linkoptions
+		{
+			--"`pkg-config --libs sdl`",
+		}
+	
+	configuration "linux"
+		targetname  "etxreal-dedicated"
+		files
+		{
+			"unix/linux_signals.c",
+			"unix/unix_main.c",
+			"unix/unix_net.c",
+			"unix/unix_shared.c",
+		}
+		--buildoptions
+		--{
+		--	"-pthread"
+		--}
+		links
+		{
+			"dl",
+			"m",
 		}
 		defines
 		{
